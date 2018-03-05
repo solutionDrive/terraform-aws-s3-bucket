6d7105338827fce92bad66929d54d135c93291fd
Matthias Alt <alt@solutiondrive.de>
2018-03-05 11:02:05 +0100

    restored policy document within the module, since we need to know the arn of the bucket for the bucket policy. inject RoleName and account-id instead

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "sd-infrastructure-up-and-running-state-${var.account_id}"
+  bucket = "${var.bucket_name}"
 
   versioning {
-    enabled = true
+    enabled = "${var.bucket_versioning}"
   }
 
   lifecycle {
@@ -10,8 +10,41 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+data "aws_iam_policy_document" "remote-backend-access" {
+  statement {
+    actions = [
+      "${var.bucket_actions}"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.terraform_state.arn}"
+    ]
+    principals {
+      identifiers = [
+        "arn:aws:iam::${var.bucket_account_id}:role/${var.bucket_role_name}"
+      ]
+      type = "AWS"
+    }
+  }
+
+  statement {
+    actions = [
+      "${var.bucket_object_actions}"
+    ]
+
+    principals {
+      identifiers = [
+        "arn:aws:iam::${var.bucket_account_id}:role/${var.bucket_role_name}"
+      ]
+      type = "AWS"
+    }
+    resources = [
+      "${aws_s3_bucket.terraform_state.arn}/*"
+    ]
+  }
+}
 
 resource "aws_s3_bucket_policy" "terraform_state" {
   bucket = "${aws_s3_bucket.terraform_state.bucket}"
-  policy = "${data.aws_iam_policy_document.remote-backend-access.json}"
+  policy = "${var.bucket_policy}"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,40 @@
+variable "bucket_account_id" {}
+
+variable "bucket_actions" {
+  description = "list of s3 actions allowed on bucket level, like List"
+  type = "list"
+  default = [
+    "s3:ListBucket"
+  ]
+}
+
+variable "bucket_name" {
+  type = "string"
+  description = "Name of the bucket"
+}
+
+variable "bucket_object_actions" {
+  description = "list of s3 actions allowed on individual bucket objects"
+  type = "list"
+  default = [
+    "s3:PutObject",
+    "s3:GetObject",
+    "s3:DeleteObject"
+  ]
+}
+
 variable "bucket_policy" {
   type = "string"
   description = "Policy JSON to attach to the bucket"
+}
+
+variable "bucket_prevent_destruction" {
+  description = "Bool value, whether the bucket should be protected from destruction"
+}
+
+variable "bucket_role_name" {}
+
+variable "bucket_versioning" {
+  description = "Bool value, whether the versioning for the bucket should be turned on"
+  default = false
 }
